5fc500bf86246e22f682a28cac88f6a3c5817e8f
abhishekmynam <abhishek.mynam@gmail.com>
2016-12-31 09:33:39 -0500

    Log file creation and unit test case
    new line not working, has to be fixed

---
+++ Solution/CFHCentralControllerServer/src/ConfigurationRepository/LogFileCreator.go
@@ -0,0 +1,40 @@
+package ConfigurationRepository
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func LogError(message string){
+	var today = time.Now().Format("01-02-2006")
+	var ErrorPath = "C:/Users/abhis/OneDrive/Documents/GitHub/CareFreeHomeLogs/CFHErrorLog" +today+".txt"
+	createFile(ErrorPath)
+	writeFile(ErrorPath, message)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+}
+func createFile(path string) {
+	var _, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		checkError(err)
+		defer file.Close()
+	}
+}
+
+func writeFile(path string, message string) {
+	var file, err = os.OpenFile(path, os.O_APPEND, 0644)
+	checkError(err)
+	var message1 = "\n " + message
+	_, err = file.WriteString(message1)
+	checkError(err)
+	err = file.Sync()
+	checkError(err)
+}
--- Solution/CFHCentralControllerServer/src/UnitTestCases/UnitTestCase.go
+++ Solution/CFHCentralControllerServer/src/UnitTestCases/UnitTestCase.go
@@ -3,12 +3,23 @@ package main
 import (
 	"LogicRepository"
 	"fmt"
-
+	"time"
+	"ConfigurationRepository"
 )
 
 func main() {
+	//getOutliers()
+	errorLogFileCheck()
+}
+
+func getOutliers(){
 	DataList := []float64{3, 5, 3, 11, 6, 7, 4, 6, 9, 0, 4, 5, 21, 22, 23, -12, -13}
 	outliersList :=LogicRepository.GetOutliers(DataList)
 	fmt.Println(outliersList)
+}
+
+func errorLogFileCheck(){
+	var message = time.Now().String()+" -- this is check for error logs 2"
+	ConfigurationRepository.LogError(message)
 }
 
