26e4176da29f18ec862d6a7125bee75f834937dc
abhishekmynam <abhishek.mynam@gmail.com>
2017-01-04 20:58:04 -0500

    react addition

--- Solution/CFHWebApp/src/components/LoginPage.js
+++ Solution/CFHWebApp/src/components/LoginPage.js
@@ -4,3 +4,15 @@
 "use strict";
 
 var React = require('react');
+
+var LoginPage = React.createClass({
+    render: function() {
+        return (
+            <div className = "jumbotron" >
+                <h1> CFH login page </h1>
+            </div>
+        );
+    }
+});
+
+module.exports=LoginPage;
